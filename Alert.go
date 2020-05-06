b8196a677a694110775c6e85b70c2aae4ef5c6a4
pmbibe <53257014+pmbibe@users.noreply.github.com>
2020-05-06 11:34:54 +0700

    Update Alert.go

--- Alert.go
+++ Alert.go
@@ -7,6 +7,46 @@ import(
 	_ "github.com/go-sql-driver/mysql"
 )
 
+func checkServiceRunning(service string, server string, lastStatusService uint, db *sql.DB) {
+	serviceName := "./exitCode.sh " + service + " " + server + " ;echo $?"
+	StatusCode := exec.Command("sh", "-c", serviceName)
+	statusCode, _ := StatusCode.Output()
+	sttCode := string(statusCode)
+	if sttCode == "0\n" && (lastStatusService != 0) {
+		log.Print("Service %s on %s is Running", service,server)
+		slect, _ :=db.Query(changeStatustoOK(service,server))
+		defer slect.Close()
+	} else if sttCode != "0\n" && (lastStatusService != 1) {
+		log.Print("Service %s on %s is Dead", service,server)
+		slect, _ :=db.Query(changeStatustoFail(service,server))
+		defer slect.Close()
+	}
+
+}
+
+func main(){
+
+db := connect()
+defer db.Close()
+for {
+
+slect, _ := db.Query("SELECT * FROM monitor")
+
+defer slect.Close()
+type Tag struct {
+	HOSTNAME string
+	SERVICE string
+	STATUSCODE uint
+}
+for slect.Next(){
+	var tag Tag
+	_ = slect.Scan(&tag.HOSTNAME,&tag.SERVICE,&tag.STATUSCODE)
+	checkServiceRunning(tag.SERVICE, tag.HOSTNAME, tag.STATUSCODE, db)
+	
+}
+}
+
+
 }
 func connecttoDB(user,password,hostname,port,database string ) string {
 	return user +":"+password +"@" + "tcp(" + hostname + ":" +port +")/"+ database
@@ -25,40 +65,3 @@ func connect() *sql.DB {
 	db, _ := sql.Open("mysql", connecttoDB("root","minhduc7b","192.168.141.204","3306","monitor_byGo"))
 	return db
 }
-func checkServiceRunning(service string, server string, lastStatusService uint, db *sql.DB) {
-	serviceName := "./exitCode.sh " + service + " " + server + " ;echo $?"
-	StatusCode := exec.Command("sh", "-c", serviceName)
-	statusCode, _ := StatusCode.Output()
-	sttCode := string(statusCode)
-	if sttCode == "0\n" && (lastStatusService != 0) {
-		log.Printf("Service %s on %s: Dead -> Running", service,server)
-		slect, _ :=db.Query(changeStatustoOK(service,server))
-		defer slect.Close()
-	} else if sttCode != "0\n" && (lastStatusService != 1) {
-		log.Printf("Service %s on %s: Running ->  Dead", service,server)
-		slect, _ :=db.Query(changeStatustoFail(service,server))
-		defer slect.Close()
-	}
-
-}
-
-func main(){
-
-db := connect()
-defer db.Close()
-for {
-slect, _ := db.Query("SELECT * FROM monitor")
-defer slect.Close()
-type Tag struct {
-	HOSTNAME string
-	SERVICE string
-	STATUSCODE uint
-}
-for slect.Next(){
-	var tag Tag
-	_ = slect.Scan(&tag.HOSTNAME,&tag.SERVICE,&tag.STATUSCODE)
-	checkServiceRunning(tag.SERVICE, tag.HOSTNAME, tag.STATUSCODE, db)
-	
-}
-}
-
